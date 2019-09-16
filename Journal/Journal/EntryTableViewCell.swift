e5b9e2bc5c6f482fc62be399f92ea8aa88a78eec
William Chen <chen.will7@gmail.com>
2019-09-16 14:12:51 -0700

    Finished connecting outlets to tableviewcell

---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by William Chen on 9/16/19.
+//  Copyright © 2019 William Chen. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    @IBOutlet weak var boldLabel: UILabel!
+    
+    @IBOutlet weak var journaltextField: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeStamp: UILabel!
+}
