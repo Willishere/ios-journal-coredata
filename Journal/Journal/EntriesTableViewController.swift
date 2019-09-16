06cded3881709a9d5c7e9fd077ee378c1f361b9a
William Chen <chen.will7@gmail.com>
2019-09-16 13:11:18 -0700

    finished labels in cell

---
+++ Journal/Journal/EntriesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EntriesTableViewController.swift
+//  Journal
+//
+//  Created by William Chen on 9/16/19.
+//  Copyright © 2019 William Chen. All rights reserved.
+//
+
+import UIKit
+
+class EntriesTableViewController: UITableViewController {
+
+}
