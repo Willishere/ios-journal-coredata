591c75e4a940e19ec06fd830cd945c3194c9a023
William Chen <chen.will7@gmail.com>
2019-09-18 12:55:39 -0700

    finished adding identifier in Entry+Convenience

--- Journal/Journal/Models/Entry+Convenience.swift
+++ Journal/Journal/Models/Entry+Convenience.swift
@@ -17,12 +17,12 @@ enum EntryMood: String, CaseIterable{
 }
 
 extension Entry {
-    convenience init(title: String, bodyText: String, mood: EntryMood, context: NSManagedObjectContext) {
+    convenience init(title: String, bodyText: String, mood: EntryMood, identifier: UUID = UUID(), context: NSManagedObjectContext) {
         self.init(context: context)
         self.title = title
         self.bodyText = bodyText
         self.timeStamp = Date()
-        self.identifier = "Entry\(Int.random(in: 1...5000))"
+        self.identifier = identifier
         self.mood = mood.rawValue
         
     }
